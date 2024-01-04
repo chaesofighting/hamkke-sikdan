9573cafe5b471e4c3f6203828fc16df47a5a61fc
Lee-Yeonjoo <121855519+Lee-Yeonjoo@users.noreply.github.com>
2024-01-04 11:46:44 +0900

    Add files via upload

---
+++ hamkke-sikdan/src/main/java/chaesofighting/hamkkesikdan/HamkkeSikdanApplication.java
@@ -0,0 +1,13 @@
+package chaesofighting.hamkkesikdan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HamkkeSikdanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HamkkeSikdanApplication.class, args);
+	}
+
+}
